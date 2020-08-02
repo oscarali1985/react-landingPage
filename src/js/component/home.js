aeef4094c208e1532bc7ec8e5eaf7f795d14cc21
oscarali1985 <oscarali1985@hotmail.com>
2020-08-01 23:13:14 -0400

    add Jumbo

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -14,64 +14,6 @@ export function Home(props) {
 	const [color, setColor] = useState("");
 	return (
 		<div>
-			<label htmlFor="name">{"Nombre:"}</label>
-			<input
-				type="text"
-				name="name"
-				onChange={event => {
-					setName(event.target.value);
-				}}
-				value={name}
-			/>
-			<label htmlFor="color">{"Color:"}</label>
-			<input
-				type="text"
-				name="color"
-				onChange={event => {
-					setColor(event.target.value);
-				}}
-				value={color}
-			/>
-			<button
-				className="btn btn-primary"
-				onClick={e => {
-					if (name != "" && color != "") {
-						let newPerson = {
-							name: name,
-							color: color
-						};
-						// copiar people
-						// let newPeople = [];
-						// for (let person of people) {
-						// 	newPeople.push(person);
-						// }
-						// newPeople.push(newPerson);
-						// setPeople de la copia
-						// spread operator ...
-						setPeople([...people, newPerson]);
-						setName("");
-						setColor("");
-					} else {
-						alert(
-							"Hola, no se pueden crear elementos sin datos..."
-						);
-					}
-				}}>
-				{"Agregar"}
-			</button>
-
-			{people.map((person, index) => {
-				// renderizar Card para esta persona, con su nombre y su color
-				return (
-					<Card
-						key={index}
-						name={person.name}
-						color={person.color}
-						imagen={img500}
-					/>
-				);
-			})}
-
 			<Jumbotron />
 
 			<div className="row">
